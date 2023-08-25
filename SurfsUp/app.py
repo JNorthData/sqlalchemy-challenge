a72742b12a8831e1cc04e6068f11deeeaeda65ae
Jeff North <jnorthdata@gmail.com>
2023-08-25 12:24:55 -0700

    tutoring help

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -5,35 +5,32 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import pandas as pd
+import datetime as dt
+import flask_sqlalchemy
+import os 
+
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 
 # create engine to hawaii.sqlite
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+
+
+# don't use global variable ^
+
 
 # reflect an existing database into a new model
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-# Get the table names from the database
-table_names = Base.classes.keys()
 
 # Save references to each table
-M_base = Base.classes.measurement
-S_base = Base.classes.station
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-# Import Measurement data
-measure_col_names = [column.key for column in M_base.__table__.columns]
-measure_rows = session.query(M_base).all()
-measure_data = [m.__dict__ for m in measure_rows]
-Measurement = pd.DataFrame(measure_data, columns=measure_col_names)
-
-# Import Station data
-station_col_names = [column.key for column in S_base.__table__.columns]
-station_rows = session.query(S_base).all()
-station_data = [s.__dict__ for s in station_rows]
-Stations = pd.DataFrame(station_data, columns=station_col_names)
 
 # create Flask app
 app = Flask(__name__)
@@ -41,26 +38,45 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page, homey!"
+    routes = [rule.rule for rule in app.url_map.iter_rules() if "static" not in rule.rule]
+    routes_html = "\n".join([f"<p style='font-size: 30px;'>{url}</p>" for url in routes])
+    return render_template("home.html", routes_html=routes_html)
 
-@app.route("/second")
 
-def second():
-    print("Server received request for SECOND page...")
-    return "Welcome to the SECOND page!"
-
-@app.route("/measurement")
-def display_measurement():
-    mcol = Measurement.columns.tolist()  # Get column names
-    mrows = Measurement.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
-    return render_template("measurement.html", columns=mcol, rows=mrows)
-
-@app.route("/station")
+@app.route("/api/v1.0/station")
 def display_station():
-    scol = Station.columns.tolist()  # Get column names
-    srows = Station.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
-    return render_template("station.html", columns=scol, rows=srows)
+    session = Session(engine)
 
+
+
+
+    session.close()
+    return jsonify()
+
+
+@app.route("/api/v1.0/measurement")
+def display_measurement():
+    session = Session(engine)
+
+    # Find the most recent date in the data set.
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+
+    # Calculate the date one year previous
+    last_date = dt.date.fromisoformat(most_recent_date)
+    one_year_previous = last_date - dt.timedelta(days=365)
+
+    # Query data from Measurement table 
+    row_data = session.query(Measurement.date, func.avg(Measurement.prcp))\
+        .filter(Measurement.date > one_year_previous)\
+        .group_by(Measurement.date)\
+        .order_by(Measurement.date)\
+        .all()
+
+    measurements = {}
+    for r in row_data:
+        measurements[r[0]] = r[1]
+
+    session.close()
+    return jsonify(measurements)
 
 app.run()
